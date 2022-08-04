5187fb297532be12538e1eff04514da914162d52
Rizan Qardafil <rizanqardafil14@gmail.com>
2022-08-04 23:20:59 +0700

    Zoom Clone

---
+++ components/ContactsMenu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from "react-native"
+function ContactsMenu() {
+  return (
+    <View>
+          <Text>Contact Menu</Text>
+    </View>
+  )
+}
+
+export default ContactsMenu
